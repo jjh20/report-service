01b75cd995408aa80477c10b7d1040e86cf29a57
jjh20 <jimenezherasmej@gmail.com>
2026-07-24 14:43:18 -0400

    Cambio en el account

--- Account.js
+++ Account.js
@@ -1,4 +1,9 @@
-MONGO_URI=mongodb://mongo-db:27017/banking
-RABBITMQ_URL=amqp://guest:guest@rabbitmq-qa:5672
-MAX_MOVIMIENTOS_PERMITIDOS=5000
-PORT=3000
+const mongoose = require('mongoose');
+
+const accountSchema = new mongoose.Schema({
+    accountNumber: { type: String, required: true, unique: true },
+    owner: { type: String, required: true },
+    balance: { type: Number, required: true, default: 0 },
+});
+
+module.exports = mongoose.model('Account', accountSchema);
